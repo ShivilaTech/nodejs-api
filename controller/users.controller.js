c988b0c4667f21c9ac39a3e4ae57fe3805716129
krishna <kkg477@gmail.com>
2023-08-14 21:31:30 +0530

    update code

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -18,7 +18,7 @@ const { messagesById } = require("./chat.controller");
       const user = await model.getuser(email_or_phone)
 
       const token = jwt.sign({
-        data: 'Token Data'}, 
+        data:email_or_phone}, 
         'ourSecretKey',{ expiresIn: '10m' }   
       ); 
       if(user){
@@ -180,11 +180,11 @@ const { messagesById } = require("./chat.controller");
 
   };
 
-  exports.verify =async (req,res) =>{
+  exports.verify = async (req,res) =>{
     const {token} = req.params;
   
   
-    jwt.verify(token, 'ourSecretKey', function(err, decoded) {
+    jwt.verify(token, 'ourSecretKey', async (err, decoded) => {
         if (err) {
             console.log(err);
            // res.send("Email verification failed, possibly the link is invalid or expired");
@@ -192,6 +192,160 @@ const { messagesById } = require("./chat.controller");
         }
         else {
           //  res.send("Email verifified successfully");
+
+             message=`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+             <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+             
+             <head>
+                 <meta charset="UTF-8">
+                 <meta content="width=device-width, initial-scale=1" name="viewport">
+                 <meta name="x-apple-disable-message-reformatting">
+                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                 <meta content="telephone=no" name="format-detection">
+                 <title></title>
+                 <!--[if (mso 16)]>
+                 <style type="text/css">
+                 a {text-decoration: none;}
+                 </style>
+                 <![endif]-->
+                 <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
+                 <!--[if gte mso 9]>
+             <xml>
+                 <o:OfficeDocumentSettings>
+                 <o:AllowPNG></o:AllowPNG>
+                 <o:PixelsPerInch>96</o:PixelsPerInch>
+                 </o:OfficeDocumentSettings>
+             </xml>
+             <![endif]-->
+             </head>
+             
+             <body>
+                 <div class="es-wrapper-color">
+                     <!--[if gte mso 9]>
+                   <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+                     <v:fill type="tile" color="#ffffff"></v:fill>
+                   </v:background>
+                 <![endif]-->
+                     <table class="es-wrapper" style="background-position: center top;" width="100%" cellspacing="0" cellpadding="0">
+                         <tbody>
+                             <tr>
+                                 <td class="esd-email-paddings" valign="top">
+                                     <table cellpadding="0" cellspacing="0" class="es-content esd-header-popover" align="center">
+                                         <tbody>
+                                             <tr>
+                                                 <td class="es-adaptive esd-stripe" esd-custom-block-id="15609" align="center">
+                                                     <table class="es-content-body" style="background-color: transparent;" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center">
+                                                         <tbody>
+                                                             <tr>
+                                                                 <td class="esd-structure es-p10" align="left">
+                                                                     <!--[if mso]><table width="580"><tr><td width="280" valign="top"><![endif]-->
+                                                                  
+                                                                     <!--[if mso]></td><td width="20"></td><td width="280" valign="top"><![endif]-->
+                                                                  
+                                                                     <!--[if mso]></td></tr></table><![endif]-->
+                                                                 </td>
+                                                             </tr>
+                                                         </tbody>
+                                                     </table>
+                                                 </td>
+                                             </tr>
+                                         </tbody>
+                                     </table>
+                                     <table class="es-header" cellspacing="0" cellpadding="0" align="center">
+                                         <tbody>
+                                             <tr>
+                                                 <td class="esd-stripe" esd-custom-block-id="15610" align="center">
+                                                     <table class="es-header-body" style="background-color: transparent;" width="600" cellspacing="0" cellpadding="0" align="center">
+                                                         <tbody>
+                                                             <tr>
+                                                                 <td class="esd-structure" align="left">
+                                                                     <table width="100%" cellspacing="0" cellpadding="0">
+                                                                         <tbody>
+                                                                             <tr>
+                                                                                 <td class="esd-container-frame" width="600" valign="top" align="center">
+                                                                                     <table width="100%" cellspacing="0" cellpadding="0">
+                                                                                         <tbody>
+                                                                                             <tr>
+                                                                                                 <td class="esd-block-image es-p20b" align="center" style="font-size:0"><a href="" alt style="display: block;" width="154"></a></td>
+                                                                                             </tr>
+                                                                                         </tbody>
+                                                                                     </table>
+                                                                                 </td>
+                                                                             </tr>
+                                                                         </tbody>
+                                                                     </table>
+                                                                 </td>
+                                                             </tr>
+                                                         </tbody>
+                                                     </table>
+                                                 </td>
+                                             </tr>
+                                         </tbody>
+                                     </table>
+                                     <table class="es-content" cellspacing="0" cellpadding="0" align="center">
+                                         <tbody>
+                                             <tr>
+                                                 <td class="esd-stripe" align="center">
+                                                     <table class="es-content-body" style="background-color: transparent;" width="600" cellspacing="0" cellpadding="0" align="center">
+                                                         <tbody>
+                                                             <tr>
+                                                                 <td class="esd-structure" align="left">
+                                                                     <table width="100%" cellspacing="0" cellpadding="0">
+                                                                         <tbody>
+                                                                             <tr>
+                                                                                 <td class="esd-container-frame" width="600" valign="top" align="center">
+                                                                                     <table style="border-radius: 3px; border-collapse: separate; background-color: #fcfcfc;" width="100%" cellspacing="0" cellpadding="0" bgcolor="#fcfcfc">
+                                                                                         <tbody>
+                                                                                             <tr>
+                                                                                                 <td class="esd-block-text es-m-txt-l es-p30t es-p20r es-p20l" align="left">
+                                                                                                     <h2 style="color: #333333;">Welcome!</h2>
+                                                                                                 </td>
+                                                                                             </tr>
+                                                                                             <tr>
+                                                                                                 <td class="esd-block-text es-p10t es-p20r es-p20l" bgcolor="#fcfcfc" align="left">
+                                                                                                     <p><b>Hi  we’re glad you’re here! You can enjoy purchases and discover new discounts every week.</b><br></p>
+                                                                                                 </td>
+                                                                                             </tr>
+                                                                                         </tbody>
+                                                                                     </table>
+                                                                                 </td>
+                                                                             </tr>
+                                                                         </tbody>
+                                                                     </table>
+                                                                 </td>
+                                                             </tr>
+                                                             <tr>
+                                                                 <td class="esd-structure es-p30t es-p20r es-p20l" style="background-color: #fcfcfc;" esd-custom-block-id="15791" bgcolor="#fcfcfc" align="left">
+                                                                 
+                                                                 </td>
+                                                             </tr>
+                                                         </tbody>
+                                                     </table>
+                                                 </td>
+                                             </tr>
+                                         </tbody>
+                                     </table>
+                              
+                                  
+                                 </td>
+                             </tr>
+                         </tbody>
+                     </table>
+                 </div>
+             </body>
+             
+             </html>`
+
+             console.log(decoded.data) 
+
+             mailConfigurations=({
+              to:decoded.data,
+              subject:"WELCOME",
+              message:message
+            })
+             
+             await comman.sendmail(mailConfigurations)
+             
 
             res.render('verify',{ status :"sussess" });
         }
@@ -206,7 +360,7 @@ const { messagesById } = require("./chat.controller");
   
      
     const token = jwt.sign({
-      data: 'Token Data'}, 
+      data: email}, 
       'ourSecretKey',{ expiresIn: '10m' }   
     ); 
 
