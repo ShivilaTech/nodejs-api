bfa62f5e90db87752c87bf590fd8202b60beaa4b
krishna <kkg477@gmail.com>
2023-08-16 16:10:49 +0530

    update code

--- controller/banner.controller.js
+++ controller/banner.controller.js
@@ -4,8 +4,11 @@ const model = require("../models/banner.model");
   exports.banners = async ( req, res) => {
 
     try {
-      const data = await model.bannersGet();
-      res.send(data);
+      const data = await model.home_banner1_images();
+      const data1 = await model.home_banner2_images();
+      const data2 = await model.home_banner3_images();
+      const data3 = await model.home_banner4_images();
+      res.send({home1:data,home2:data1,home3:data2,home4:data3});
       
      } catch(err) {
       console.log(err);     
--- models/banner.model.js
+++ models/banner.model.js
@@ -1,14 +1,96 @@
 const db = require("./db.js");
+const comman = require("./comman.model.js");
 
-exports.bannersGet = () =>{
-    return new Promise((resolve, reject)=>{   
 
-      db.query(`SELECT * FROM banners`,  (error, elements)=>{
-            if(error){
-                return reject(error);
-            }
-            return resolve(elements);
-        });
-    });
+exports.home_banner1_images = async () =>{
+      
+    try {
+      const data = await comman.businessSettings('home_banner1_images');
+      const arr=JSON.parse(data[0].value);  
+      const ids=arr.toString();
   
-};
+      return new Promise((resolve, reject)=>{   
+  
+        db.query(`SELECT * FROM uploads where id in (${ids})  `,  (error, elements)=>{
+              if(error){
+                  return reject(error);
+              }
+              return resolve(elements);
+          });
+      });
+      
+     } catch(err) {
+      console.log(err);     
+     }
+    
+  };
+
+  exports.home_banner2_images = async () =>{
+      
+    try {
+      const data = await comman.businessSettings('home_banner2_images');
+      const arr=JSON.parse(data[0].value);  
+      const ids=arr.toString();
+  
+      return new Promise((resolve, reject)=>{   
+  
+        db.query(`SELECT * FROM uploads where id in (${ids})  `,  (error, elements)=>{
+              if(error){
+                  return reject(error);
+              }
+              return resolve(elements);
+          });
+      });
+      
+     } catch(err) {
+      console.log(err);     
+     }
+    
+  };
+
+  exports.home_banner3_images = async () =>{
+      
+    try {
+      const data = await comman.businessSettings('home_banner3_images');
+      const arr=JSON.parse(data[0].value);  
+      const ids=arr.toString();
+  
+      return new Promise((resolve, reject)=>{   
+  
+        db.query(`SELECT * FROM uploads where id in (${ids})  `,  (error, elements)=>{
+              if(error){
+                  return reject(error);
+              }
+              return resolve(elements);
+          });
+      });
+      
+     } catch(err) {
+      console.log(err);     
+     }
+    
+  };
+
+  
+  exports.home_banner4_images = async () =>{
+      
+    try {
+      const data = await comman.businessSettings('home_banner4_images');
+      const arr=JSON.parse(data[0].value);  
+      const ids=arr.toString();
+  
+      return new Promise((resolve, reject)=>{   
+  
+        db.query(`SELECT * FROM uploads where id in (${ids})  `,  (error, elements)=>{
+              if(error){
+                  return reject(error);
+              }
+              return resolve(elements);
+          });
+      });
+      
+     } catch(err) {
+      console.log(err);     
+     }
+    
+  };
